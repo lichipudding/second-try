b2b58a28d4b0eef4beca53a159934196de218299
lichipudding <kanani.aydan@gmail.com>
2021-09-15 17:01:25 +0200

    changes in enemy and bullet

--- Assets/scripts/bullet.cs
+++ Assets/scripts/bullet.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
+    public int damage = 50; //how much damage the bullet is doing
+    public Rigidbody2D rb;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision) //destroy bullet on collision
     {
         if (collision.gameObject.CompareTag("Enemy"))
-            {
-            Destroy(gameObject);
+          {
+              Destroy(gameObject);
         }
     }
+
+    private void OnCollisionEnter2D(Collider2D hitInfo) // bullet damages enemy
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.takeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+
 }
